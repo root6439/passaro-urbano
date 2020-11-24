03b42e4d53b7853e9bd47e54f216b1651449d207
root6439 <nicolas.soaresh.98@gmail.com>
2020-11-24 10:15:31 -0300

    Template da página pronto, criação de oferta.model.ts e inserção de ofertas na tela com base na classe Oferta

---
+++ src/app/shared/oferta.model.ts
@@ -0,0 +1,12 @@
+export class Oferta {
+
+  public id: Number;
+	public categoria: String;
+	public titulo: String;
+	public descricao_oferta: String; 
+	public anunciante: String;
+	public valor: Number;
+	public destaque: Boolean
+	public imagens: Array<Object>;
+
+}
