108e43938f03bf811663e6af44b89fc5e6ffbea1
root6439 <nicolas.soaresh.98@gmail.com>
2022-09-01 20:13:15 -0300

    ajuste polyfils

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,6 +61,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-import 'intl'
-import 'intl/locale-data/jsonp/pt-Br'
